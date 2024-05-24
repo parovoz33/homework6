9434bdee049b61efa8f6e7b348217f8dd03c9521
parovoz33 <parovoz33@MAIL.RU>
2024-05-24 15:25:05 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,12 @@
+my_list=['scooter','bicycle','car','train','plane']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:])
+my_list[2]='metro'
+print(my_list)
+my_dict={'scooter':'самокат','bicycle':'велосипед','car':'автомобиль','train':'поезд','plane':'самолет'}
+print(my_dict)
+print(my_dict['car'])
+my_dict['car']='лимузин'
+print(my_dict)
